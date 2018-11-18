9e426f7f937a189434376afa467eb58050589908
hongbao liu <hongbaoliu@hongbaodeMBP.lan>
2018-11-18 13:38:32 +0800

    修复bingo安装时报错的问题

--- examples/main.go
+++ examples/main.go
@@ -23,7 +23,6 @@ func setRoute() *bingo_router.Route {
 
 		})
 
-
 		b.NewRoute().Get("/test2").Target(func(c *bingo_router.Context) {
 			fmt.Fprint(c.Writer, "hello test2")
 		}).Middleware(func(c *bingo_router.Context, next func(c *bingo_router.Context)) {
@@ -41,16 +40,15 @@ func main() {
 	// 创建一个路由
 	r := bingo_router.New()
 
-
 	// 得到一个路由
 	rr := setRoute()
 
-	r2 := bingo_router.NewRoute().Get("/test").Middleware(func(c *bingo_router.Context, next func(c *bingo_router.Context)) {
-		fmt.Fprint(c.Writer,"middleware")
-		next(c)
-	}).Target(func(c *bingo_router.Context) {
-		fmt.Fprintln(c.Writer,"target")
-	})
+	//r2 := bingo_router.NewRoute().Get("/test").Middleware(func(c *bingo_router.Context, next func(c *bingo_router.Context)) {
+	//	fmt.Fprint(c.Writer,"middleware")
+	//	next(c)
+	//}).Target(func(c *bingo_router.Context) {
+	//	fmt.Fprintln(c.Writer,"target")
+	//})
 	// 挂载路由方法，遍历传入的所有路由，然后遍历每个路由里的子路由，并设定路径等，设定中间件等
 	r.Mount(rr)
 
